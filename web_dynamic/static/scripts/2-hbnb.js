144e5d4fb8a06c11863f02735533378429b0118e
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-22 15:00:55 +0200

    add class change

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
       amenities.splice($.inArray($(this).attr('data-id'), amenities), 1);
     }
   });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
